3553b274403f28382e1821572c4d62e61208265f
Beka Fekadu <98936162+Arcane11@users.noreply.github.com>
2023-11-28 14:24:12 +0300

    corrected the errors.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for x in range(i + 1, 10):
-        if i == 8:
-            print("{}{}".format(i, x))
-        else:
-            print("{}{}".format(i, x), end=", ")
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
